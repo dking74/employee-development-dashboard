173226d3a2dd35179f05b1e120a58b843f06f342
Devon King <4daking7@gmail.com`>
2021-05-01 11:22:40 -0500

    Fix prod config

--- backend/src/v1/validations/certifications.ts
+++ backend/src/v1/validations/certifications.ts
@@ -3,11 +3,11 @@ import Joi from 'joi';
 export const createCertificationSchema: Joi.ObjectSchema = Joi.object({
   name: Joi.string().required(),
   description: Joi.string().required().allow(''),
-  link: Joi.string().optional().pattern(/^http/).default('')
+  attachment_url: Joi.string().optional().pattern(/^http/).default('')
 }).min(1).max(3);
 
 export const updateCertificationSchema: Joi.ObjectSchema = Joi.object({
   name: Joi.string().optional(),
-  description: Joi.string().optional(),
-  link: Joi.string().optional().pattern(/^http/)
+  description: Joi.string().optional().allow(''),
+  attachment_url: Joi.string().optional().pattern(/^http/)
 }).min(1);
--- backend/src/v1/validations/goals.ts
+++ backend/src/v1/validations/goals.ts
@@ -1,16 +1,20 @@
 import Joi from 'joi';
 
 export const createGoalBodySchema: Joi.ObjectSchema = Joi.object({
+  title: Joi.string().required(),
   summary: Joi.string().required(),
-  to_be_completed_date: Joi.date().required(),
-  completed_date: Joi.date(),
+  to_be_completed_date: Joi.date()
+    .when('status', { is: 'pending', then: Joi.required(), otherwise: Joi.allow('') }),
+  completion_date: Joi.date()
+    .when('status', { is: 'completed', then: Joi.required(), otherwise: Joi.allow('') }),
   status: Joi.string().allow('submitted', 'pending', 'completed')
-}).min(2).max(4);
+}).min(2).max(5);
 
 export const updateGoalBodySchema: Joi.ObjectSchema = Joi.object({
   goal_id: Joi.forbidden(),
+  title: Joi.string(),
   summary: Joi.string(),
   to_be_completed_date: Joi.date(),
   completed_date: Joi.date(),
   status: Joi.string().allow('submitted', 'pending', 'completed')
-}).min(1).max(4);
+}).min(1).max(5);
